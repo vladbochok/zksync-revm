6246dbeffdefef69a4162eda0bb4463bb2415619
Vlad Bochok <vladbochok1@gmail.com>
2025-10-02 10:13:19 +0200

    Fork of OP revm

---
+++ src/evm.rs
@@ -0,0 +1,164 @@
+//! Contains the `[OpEvm]` type and its implementation of the execution EVM traits.
+use crate::precompiles::OpPrecompiles;
+use revm::{
+    context::{ContextError, ContextSetters, Evm, FrameStack},
+    context_interface::ContextTr,
+    handler::{
+        evm::FrameTr,
+        instructions::{EthInstructions, InstructionProvider},
+        EthFrame, EvmTr, FrameInitOrResult, ItemOrResult, PrecompileProvider,
+    },
+    inspector::{InspectorEvmTr, JournalExt},
+    interpreter::{interpreter::EthInterpreter, InterpreterResult},
+    Database, Inspector,
+};
+
+/// Optimism EVM extends the [`Evm`] type with Optimism specific types and logic.
+#[derive(Debug, Clone)]
+pub struct OpEvm<
+    CTX,
+    INSP,
+    I = EthInstructions<EthInterpreter, CTX>,
+    P = OpPrecompiles,
+    F = EthFrame<EthInterpreter>,
+>(
+    /// Inner EVM type.
+    pub Evm<CTX, INSP, I, P, F>,
+);
+
+impl<CTX: ContextTr, INSP> OpEvm<CTX, INSP, EthInstructions<EthInterpreter, CTX>, OpPrecompiles> {
+    /// Create a new Optimism EVM.
+    pub fn new(ctx: CTX, inspector: INSP) -> Self {
+        Self(Evm {
+            ctx,
+            inspector,
+            instruction: EthInstructions::new_mainnet(),
+            precompiles: OpPrecompiles::default(),
+            frame_stack: FrameStack::new(),
+        })
+    }
+}
+
+impl<CTX, INSP, I, P> OpEvm<CTX, INSP, I, P> {
+    /// Consumed self and returns a new Evm type with given Inspector.
+    pub fn with_inspector<OINSP>(self, inspector: OINSP) -> OpEvm<CTX, OINSP, I, P> {
+        OpEvm(self.0.with_inspector(inspector))
+    }
+
+    /// Consumes self and returns a new Evm type with given Precompiles.
+    pub fn with_precompiles<OP>(self, precompiles: OP) -> OpEvm<CTX, INSP, I, OP> {
+        OpEvm(self.0.with_precompiles(precompiles))
+    }
+
+    /// Consumes self and returns the inner Inspector.
+    pub fn into_inspector(self) -> INSP {
+        self.0.into_inspector()
+    }
+}
+
+impl<CTX, INSP, I, P> InspectorEvmTr for OpEvm<CTX, INSP, I, P>
+where
+    CTX: ContextTr<Journal: JournalExt> + ContextSetters,
+    I: InstructionProvider<Context = CTX, InterpreterTypes = EthInterpreter>,
+    P: PrecompileProvider<CTX, Output = InterpreterResult>,
+    INSP: Inspector<CTX, I::InterpreterTypes>,
+{
+    type Inspector = INSP;
+
+    fn inspector(&mut self) -> &mut Self::Inspector {
+        &mut self.0.inspector
+    }
+
+    fn ctx_inspector(&mut self) -> (&mut Self::Context, &mut Self::Inspector) {
+        (&mut self.0.ctx, &mut self.0.inspector)
+    }
+
+    fn ctx_inspector_frame(
+        &mut self,
+    ) -> (&mut Self::Context, &mut Self::Inspector, &mut Self::Frame) {
+        (
+            &mut self.0.ctx,
+            &mut self.0.inspector,
+            self.0.frame_stack.get(),
+        )
+    }
+
+    fn ctx_inspector_frame_instructions(
+        &mut self,
+    ) -> (
+        &mut Self::Context,
+        &mut Self::Inspector,
+        &mut Self::Frame,
+        &mut Self::Instructions,
+    ) {
+        (
+            &mut self.0.ctx,
+            &mut self.0.inspector,
+            self.0.frame_stack.get(),
+            &mut self.0.instruction,
+        )
+    }
+}
+
+impl<CTX, INSP, I, P> EvmTr for OpEvm<CTX, INSP, I, P, EthFrame<EthInterpreter>>
+where
+    CTX: ContextTr,
+    I: InstructionProvider<Context = CTX, InterpreterTypes = EthInterpreter>,
+    P: PrecompileProvider<CTX, Output = InterpreterResult>,
+{
+    type Context = CTX;
+    type Instructions = I;
+    type Precompiles = P;
+    type Frame = EthFrame<EthInterpreter>;
+
+    fn ctx(&mut self) -> &mut Self::Context {
+        &mut self.0.ctx
+    }
+
+    fn ctx_ref(&self) -> &Self::Context {
+        &self.0.ctx
+    }
+
+    fn ctx_instructions(&mut self) -> (&mut Self::Context, &mut Self::Instructions) {
+        (&mut self.0.ctx, &mut self.0.instruction)
+    }
+
+    fn ctx_precompiles(&mut self) -> (&mut Self::Context, &mut Self::Precompiles) {
+        (&mut self.0.ctx, &mut self.0.precompiles)
+    }
+
+    fn frame_stack(&mut self) -> &mut FrameStack<Self::Frame> {
+        &mut self.0.frame_stack
+    }
+
+    fn frame_init(
+        &mut self,
+        frame_input: <Self::Frame as FrameTr>::FrameInit,
+    ) -> Result<
+        ItemOrResult<&mut Self::Frame, <Self::Frame as FrameTr>::FrameResult>,
+        ContextError<<<Self::Context as ContextTr>::Db as Database>::Error>,
+    > {
+        self.0.frame_init(frame_input)
+    }
+
+    fn frame_run(
+        &mut self,
+    ) -> Result<
+        FrameInitOrResult<Self::Frame>,
+        ContextError<<<Self::Context as ContextTr>::Db as Database>::Error>,
+    > {
+        self.0.frame_run()
+    }
+
+    #[doc = " Returns the result of the frame to the caller. Frame is popped from the frame stack."]
+    #[doc = " Consumes the frame result or returns it if there is more frames to run."]
+    fn frame_return_result(
+        &mut self,
+        result: <Self::Frame as FrameTr>::FrameResult,
+    ) -> Result<
+        Option<<Self::Frame as FrameTr>::FrameResult>,
+        ContextError<<<Self::Context as ContextTr>::Db as Database>::Error>,
+    > {
+        self.0.frame_return_result(result)
+    }
+}
---
+++ src/precompiles.rs
@@ -0,0 +1,348 @@
+//! Contains Optimism specific precompiles.
+use crate::OpSpecId;
+use revm::{
+    context::Cfg,
+    context_interface::ContextTr,
+    handler::{EthPrecompiles, PrecompileProvider},
+    interpreter::{InputsImpl, InterpreterResult},
+    precompile::{
+        self, bn254, secp256r1, Precompile, PrecompileError, PrecompileId, PrecompileResult,
+        Precompiles,
+    },
+    primitives::{hardfork::SpecId, Address, OnceLock},
+};
+use std::boxed::Box;
+use std::string::String;
+
+/// Optimism precompile provider
+#[derive(Debug, Clone)]
+pub struct OpPrecompiles {
+    /// Inner precompile provider is same as Ethereums.
+    inner: EthPrecompiles,
+    /// Spec id of the precompile provider.
+    spec: OpSpecId,
+}
+
+impl OpPrecompiles {
+    /// Create a new precompile provider with the given OpSpec.
+    #[inline]
+    pub fn new_with_spec(spec: OpSpecId) -> Self {
+        let precompiles = match spec {
+            spec @ (OpSpecId::BEDROCK
+            | OpSpecId::REGOLITH
+            | OpSpecId::CANYON
+            | OpSpecId::ECOTONE) => Precompiles::new(spec.into_eth_spec().into()),
+            OpSpecId::FJORD => fjord(),
+            OpSpecId::GRANITE | OpSpecId::HOLOCENE => granite(),
+            OpSpecId::ISTHMUS | OpSpecId::INTEROP | OpSpecId::OSAKA => isthmus(),
+        };
+
+        Self {
+            inner: EthPrecompiles {
+                precompiles,
+                spec: SpecId::default(),
+            },
+            spec,
+        }
+    }
+
+    /// Precompiles getter.
+    #[inline]
+    pub fn precompiles(&self) -> &'static Precompiles {
+        self.inner.precompiles
+    }
+}
+
+/// Returns precompiles for Fjord spec.
+pub fn fjord() -> &'static Precompiles {
+    static INSTANCE: OnceLock<Precompiles> = OnceLock::new();
+    INSTANCE.get_or_init(|| {
+        let mut precompiles = Precompiles::cancun().clone();
+        // RIP-7212: secp256r1 P256verify
+        precompiles.extend([secp256r1::P256VERIFY]);
+        precompiles
+    })
+}
+
+/// Returns precompiles for Granite spec.
+pub fn granite() -> &'static Precompiles {
+    static INSTANCE: OnceLock<Precompiles> = OnceLock::new();
+    INSTANCE.get_or_init(|| {
+        let mut precompiles = fjord().clone();
+        // Restrict bn254Pairing input size
+        precompiles.extend([bn254_pair::GRANITE]);
+        precompiles
+    })
+}
+
+/// Returns precompiles for isthumus spec.
+pub fn isthmus() -> &'static Precompiles {
+    static INSTANCE: OnceLock<Precompiles> = OnceLock::new();
+    INSTANCE.get_or_init(|| {
+        let mut precompiles = granite().clone();
+        // Prague bls12 precompiles
+        precompiles.extend(precompile::bls12_381::precompiles());
+        // Isthmus bls12 precompile modifications
+        precompiles.extend([
+            bls12_381::ISTHMUS_G1_MSM,
+            bls12_381::ISTHMUS_G2_MSM,
+            bls12_381::ISTHMUS_PAIRING,
+        ]);
+        precompiles
+    })
+}
+
+impl<CTX> PrecompileProvider<CTX> for OpPrecompiles
+where
+    CTX: ContextTr<Cfg: Cfg<Spec = OpSpecId>>,
+{
+    type Output = InterpreterResult;
+
+    #[inline]
+    fn set_spec(&mut self, spec: <CTX::Cfg as Cfg>::Spec) -> bool {
+        if spec == self.spec {
+            return false;
+        }
+        *self = Self::new_with_spec(spec);
+        true
+    }
+
+    #[inline]
+    fn run(
+        &mut self,
+        context: &mut CTX,
+        address: &Address,
+        inputs: &InputsImpl,
+        is_static: bool,
+        gas_limit: u64,
+    ) -> Result<Option<Self::Output>, String> {
+        self.inner
+            .run(context, address, inputs, is_static, gas_limit)
+    }
+
+    #[inline]
+    fn warm_addresses(&self) -> Box<impl Iterator<Item = Address>> {
+        self.inner.warm_addresses()
+    }
+
+    #[inline]
+    fn contains(&self, address: &Address) -> bool {
+        self.inner.contains(address)
+    }
+}
+
+impl Default for OpPrecompiles {
+    fn default() -> Self {
+        Self::new_with_spec(OpSpecId::ISTHMUS)
+    }
+}
+
+/// Bn254 pair precompile.
+pub mod bn254_pair {
+    use super::*;
+
+    /// Max input size for the bn254 pair precompile.
+    pub const GRANITE_MAX_INPUT_SIZE: usize = 112687;
+    /// Bn254 pair precompile.
+    pub const GRANITE: Precompile =
+        Precompile::new(PrecompileId::Bn254Pairing, bn254::pair::ADDRESS, run_pair);
+
+    /// Run the bn254 pair precompile with Optimism input limit.
+    pub fn run_pair(input: &[u8], gas_limit: u64) -> PrecompileResult {
+        if input.len() > GRANITE_MAX_INPUT_SIZE {
+            return Err(PrecompileError::Bn254PairLength);
+        }
+        bn254::run_pair(
+            input,
+            bn254::pair::ISTANBUL_PAIR_PER_POINT,
+            bn254::pair::ISTANBUL_PAIR_BASE,
+            gas_limit,
+        )
+    }
+}
+
+/// Bls12_381 precompile.
+pub mod bls12_381 {
+    use super::*;
+    use revm::precompile::bls12_381_const::{G1_MSM_ADDRESS, G2_MSM_ADDRESS, PAIRING_ADDRESS};
+
+    #[cfg(not(feature = "std"))]
+    use crate::std::string::ToString;
+
+    /// Max input size for the g1 msm precompile.
+    pub const ISTHMUS_G1_MSM_MAX_INPUT_SIZE: usize = 513760;
+    /// Max input size for the g2 msm precompile.
+    pub const ISTHMUS_G2_MSM_MAX_INPUT_SIZE: usize = 488448;
+    /// Max input size for the pairing precompile.
+    pub const ISTHMUS_PAIRING_MAX_INPUT_SIZE: usize = 235008;
+
+    /// G1 msm precompile.
+    pub const ISTHMUS_G1_MSM: Precompile =
+        Precompile::new(PrecompileId::Bls12G1Msm, G1_MSM_ADDRESS, run_g1_msm);
+    /// G2 msm precompile.
+    pub const ISTHMUS_G2_MSM: Precompile =
+        Precompile::new(PrecompileId::Bls12G2Msm, G2_MSM_ADDRESS, run_g2_msm);
+    /// Pairing precompile.
+    pub const ISTHMUS_PAIRING: Precompile =
+        Precompile::new(PrecompileId::Bls12Pairing, PAIRING_ADDRESS, run_pair);
+
+    /// Run the g1 msm precompile with Optimism input limit.
+    pub fn run_g1_msm(input: &[u8], gas_limit: u64) -> PrecompileResult {
+        if input.len() > ISTHMUS_G1_MSM_MAX_INPUT_SIZE {
+            return Err(PrecompileError::Other(
+                "G1MSM input length too long for OP Stack input size limitation".to_string(),
+            ));
+        }
+        precompile::bls12_381::g1_msm::g1_msm(input, gas_limit)
+    }
+
+    /// Run the g2 msm precompile with Optimism input limit.
+    pub fn run_g2_msm(input: &[u8], gas_limit: u64) -> PrecompileResult {
+        if input.len() > ISTHMUS_G2_MSM_MAX_INPUT_SIZE {
+            return Err(PrecompileError::Other(
+                "G2MSM input length too long for OP Stack input size limitation".to_string(),
+            ));
+        }
+        precompile::bls12_381::g2_msm::g2_msm(input, gas_limit)
+    }
+
+    /// Run the pairing precompile with Optimism input limit.
+    pub fn run_pair(input: &[u8], gas_limit: u64) -> PrecompileResult {
+        if input.len() > ISTHMUS_PAIRING_MAX_INPUT_SIZE {
+            return Err(PrecompileError::Other(
+                "Pairing input length too long for OP Stack input size limitation".to_string(),
+            ));
+        }
+        precompile::bls12_381::pairing::pairing(input, gas_limit)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::precompiles::bls12_381::{
+        run_g1_msm, run_g2_msm, ISTHMUS_G1_MSM_MAX_INPUT_SIZE, ISTHMUS_G2_MSM_MAX_INPUT_SIZE,
+        ISTHMUS_PAIRING_MAX_INPUT_SIZE,
+    };
+
+    use super::*;
+    use revm::{
+        precompile::PrecompileError,
+        primitives::{hex, Bytes},
+    };
+    use std::vec;
+
+    #[test]
+    fn test_bn254_pair() {
+        let input = hex::decode(
+            "\
+      1c76476f4def4bb94541d57ebba1193381ffa7aa76ada664dd31c16024c43f59\
+      3034dd2920f673e204fee2811c678745fc819b55d3e9d294e45c9b03a76aef41\
+      209dd15ebff5d46c4bd888e51a93cf99a7329636c63514396b4a452003a35bf7\
+      04bf11ca01483bfa8b34b43561848d28905960114c8ac04049af4b6315a41678\
+      2bb8324af6cfc93537a2ad1a445cfd0ca2a71acd7ac41fadbf933c2a51be344d\
+      120a2a4cf30c1bf9845f20c6fe39e07ea2cce61f0c9bb048165fe5e4de877550\
+      111e129f1cf1097710d41c4ac70fcdfa5ba2023c6ff1cbeac322de49d1b6df7c\
+      2032c61a830e3c17286de9462bf242fca2883585b93870a73853face6a6bf411\
+      198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2\
+      1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed\
+      090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b\
+      12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa",
+        )
+        .unwrap();
+        let expected =
+            hex::decode("0000000000000000000000000000000000000000000000000000000000000001")
+                .unwrap();
+        let outcome = bn254_pair::run_pair(&input, 260_000).unwrap();
+        assert_eq!(outcome.bytes, expected);
+
+        // Invalid input length
+        let input = hex::decode(
+            "\
+          1111111111111111111111111111111111111111111111111111111111111111\
+          1111111111111111111111111111111111111111111111111111111111111111\
+          111111111111111111111111111111\
+      ",
+        )
+        .unwrap();
+
+        let res = bn254_pair::run_pair(&input, 260_000);
+        assert!(matches!(res, Err(PrecompileError::Bn254PairLength)));
+
+        // Valid input length shorter than 112687
+        let input = vec![1u8; 586 * bn254::PAIR_ELEMENT_LEN];
+        let res = bn254_pair::run_pair(&input, 260_000);
+        assert!(matches!(res, Err(PrecompileError::OutOfGas)));
+
+        // Input length longer than 112687
+        let input = vec![1u8; 587 * bn254::PAIR_ELEMENT_LEN];
+        let res = bn254_pair::run_pair(&input, 260_000);
+        assert!(matches!(res, Err(PrecompileError::Bn254PairLength)));
+    }
+
+    #[test]
+    fn test_cancun_precompiles_in_fjord() {
+        // additional to cancun, fjord has p256verify
+        assert_eq!(fjord().difference(Precompiles::cancun()).len(), 1)
+    }
+
+    #[test]
+    fn test_cancun_precompiles_in_granite() {
+        // granite has p256verify (fjord)
+        // granite has modification of cancun's bn254 pair (doesn't count as new precompile)
+        assert_eq!(granite().difference(Precompiles::cancun()).len(), 1)
+    }
+
+    #[test]
+    fn test_prague_precompiles_in_isthmus() {
+        let new_prague_precompiles = Precompiles::prague().difference(Precompiles::cancun());
+
+        // isthmus contains all precompiles that were new in prague, without modifications
+        assert!(new_prague_precompiles.difference(isthmus()).is_empty())
+    }
+
+    #[test]
+    fn test_default_precompiles_is_latest() {
+        let latest = OpPrecompiles::new_with_spec(OpSpecId::default())
+            .inner
+            .precompiles;
+        let default = OpPrecompiles::default().inner.precompiles;
+        assert_eq!(latest.len(), default.len());
+
+        let intersection = default.intersection(latest);
+        assert_eq!(intersection.len(), latest.len())
+    }
+
+    #[test]
+    fn test_g1_isthmus_max_size() {
+        let oversized_input = vec![0u8; ISTHMUS_G1_MSM_MAX_INPUT_SIZE + 1];
+        let input = Bytes::from(oversized_input);
+
+        let res = run_g1_msm(&input, 260_000);
+
+        assert!(
+            matches!(res, Err(PrecompileError::Other(msg)) if msg.contains("input length too long"))
+        );
+    }
+    #[test]
+    fn test_g2_isthmus_max_size() {
+        let oversized_input = vec![0u8; ISTHMUS_G2_MSM_MAX_INPUT_SIZE + 1];
+        let input = Bytes::from(oversized_input);
+
+        let res = run_g2_msm(&input, 260_000);
+
+        assert!(
+            matches!(res, Err(PrecompileError::Other(msg)) if msg.contains("input length too long"))
+        );
+    }
+    #[test]
+    fn test_pair_isthmus_max_size() {
+        let oversized_input = vec![0u8; ISTHMUS_PAIRING_MAX_INPUT_SIZE + 1];
+        let input = Bytes::from(oversized_input);
+
+        let res = bls12_381::run_pair(&input, 260_000);
+
+        assert!(
+            matches!(res, Err(PrecompileError::Other(msg)) if msg.contains("input length too long"))
+        );
+    }
+}
