22cc4e42c0886c01d9e633c3ce43274db8c05c23
Vlad Bochok <vladbochok1@gmail.com>
2025-10-03 14:17:06 +0200

    debug

--- src/precompiles/deployer.rs
+++ src/precompiles/deployer.rs
@@ -87,7 +87,7 @@ where
             let bytecode = ctx.db_mut().code_by_hash(observable_bytecode_hash).expect(
                 "The bytecode is expected to be pre-loaded for any deployer precompile call",
             );
-            if bytecode.len() <= 10 {
+            if bytecode.len() >= 10 {
                 panic!("bytecode is small {:?}", address);
             }
             ctx.journal_mut().set_code(address, bytecode);
