085ae722ebf3ee33c79a64237e24652f12d2f4ed
Vlad Bochok <vladbochok1@gmail.com>
2025-10-03 11:46:20 +0200

    New interface for deployer

--- src/precompiles/deployer.rs
+++ src/precompiles/deployer.rs
@@ -7,12 +7,12 @@ use revm::{
         self, bn254, secp256r1, Precompile, PrecompileError, PrecompileId, PrecompileResult,
         Precompiles,
     },
-    primitives::{address, hardfork::SpecId, ruint::aliases::B160, Address, OnceLock, U256},
+    primitives::{address, hardfork::SpecId, ruint::aliases::B160, Address, OnceLock, U256, B256},
 };
 use std::string::String;
 
-// setDeployedCodeEVM(address,bytes) - 1223adc7
-const SET_DEPLOYED_CODE_EVM_SELECTOR: &[u8] = &[0x12, 0x23, 0xad, 0xc7];
+// setBytecodeDetailsEVM(address,bytes32,uint32,bytes32) - f6eca0b0
+pub const SET_EVM_BYTECODE_DETAILS: &[u8] = &[0xf6, 0xec, 0xa0, 0xb0];
 // Contract Deployer system hook (contract) needed for all envs (force deploy)
 pub const CONTRACT_DEPLOYER_ADDRESS: Address = address!("0000000000000000000000000000000000008006");
 
@@ -27,9 +27,8 @@ pub fn deployer_precompile_call(caller: Address, is_static: bool, gas_limit: u64
     }
     let mut selector = [0u8; 4];
     selector.copy_from_slice(&calldata[..4]);
-
     match selector {
-        s if s == SET_DEPLOYED_CODE_EVM_SELECTOR => {
+        s if s == SET_EVM_BYTECODE_DETAILS => {
             if is_static {
                 return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit));
             }
@@ -40,7 +39,7 @@ pub fn deployer_precompile_call(caller: Address, is_static: bool, gas_limit: u64
 
             // decoding according to setDeployedCodeEVM(address,bytes)
             calldata = &calldata[4..];
-            if calldata.len() < 64 {
+            if calldata.len() < 128 {
                 return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit));
             }
 
@@ -53,37 +52,21 @@ pub fn deployer_precompile_call(caller: Address, is_static: bool, gas_limit: u64
                 None => return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit)),
             };
 
+            let _bytecode_hash =
+                B256::from_slice(calldata[32..64].try_into().expect("Always valid"));
 
-            let bytecode_offset: usize = match U256::from_be_slice(&calldata[32..64]).try_into() {
-                Ok(offset) => offset,
-                Err(_) => return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit)),
-            };
-
-            let bytecode_length_encoding_end = match bytecode_offset.checked_add(32) {
-                Some(deployments_encoding_end) => deployments_encoding_end,
-                None => return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit)),
-            };
-            let bytecode_length: usize = match U256::from_be_slice(
-                &calldata[bytecode_length_encoding_end - 32..bytecode_length_encoding_end],
-            )
-            .try_into()
-            {
+            let bytecode_length: u32 = match U256::from_be_slice(&calldata[64..96]).try_into() {
                 Ok(length) => length,
                 Err(_) => return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit)),
             };
 
-            if calldata.len() < bytecode_length_encoding_end + bytecode_length {
-                return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit));
-            }
-
-            let bytecode = &calldata
-                [bytecode_length_encoding_end..bytecode_length_encoding_end + bytecode_length];
+            let observable_bytecode_hash =
+                B256::from_slice(calldata[96..128].try_into().expect("Always valid"));
 
             // Although this can be called as a part of protocol upgrade,
             // we are checking the next invariants, just in case
-            // EIP-3541: reject code starting with 0xEF.
             // EIP-158: reject code of length > 24576.
-            if !bytecode.is_empty() && bytecode[0] == 0xEF || bytecode.len() > MAX_CODE_SIZE {
+            if bytecode_length as usize > MAX_CODE_SIZE {
                 return InterpreterResult::new(InstructionResult::Revert, [].into(), Gas::new(gas_limit));
             }
 
